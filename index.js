3f3e1ebef738fa07d85395b000be74ab92a019c0
Sathyamoorthi H <ses@integra.co.in>
2023-03-21 15:27:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function isNumber(num) {
+    var numbers = /^[0-9]+$/;
+    if(typeof num ==='number')
+    return true
+    else
+    return false
+}
+
+module.exports = isNumber
---
+++ test/script.js
@@ -0,0 +1,3 @@
+const is_number = require('is_number')
+
+console.log(is_number('ijkdf'))
